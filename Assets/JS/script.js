2acc1f169668ac7cca3457a17741cded4d5a1e1b
osiyo <merewall@gmail.com>
2021-05-28 03:51:28 -0400

    Cleaning up and commenting JS. Adding favicon

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,13 @@
-// let currWeatherArray = [];
+// Creating an empty array to which searched cities can be pushed to for saving to local storage.
 let searchedCityArray = [];
-let lastSearchIndex = (searchedCityArray.length - 1)
+
+// Creating empty arrays to which 5-day forecast data can be pushed and iterated over for rendering the 5-day forecast to site
 let dailyTempArray = [];
 let dailyHumidityArray = [];
 let dailyWindArray = [];
 let dailyIconArray = [];
+
+// Creating variables to select HTML elements on the page, in which data can be rendered
 let $searchForm = $('form');
 let $cityName = $('#city-name');
 let $currDate = $('#current-date');
@@ -18,6 +21,8 @@ let $forecastTemps = $('.forecast-days .forecast-temps');
 let $forecastWinds = $('.forecast-days .forecast-winds');
 let $forecastHumiditys = $('.forecast-days .forecast-humiditys');
 let $forecastIcons = $('.forecast-days .forecast-icons');
+
+// Generating the current date and dates of the 5-day forecast
 let currentDate = moment().format('L');
 let forecastDay1 = moment(currentDate,"L").add(1, "days").format("L");
 let forecastDay2 = moment(currentDate,"L").add(2, "days").format("L");
@@ -25,6 +30,7 @@ let forecastDay3 = moment(currentDate,"L").add(3, "days").format("L");
 let forecastDay4 = moment(currentDate,"L").add(4, "days").format("L");
 let forecastDay5 = moment(currentDate,"L").add(5, "days").format("L");
 
+// Rendering the current date and 5-day forecast dates to the site on page load
 function renderDates() {
     $currDate.text(currentDate);
     $('#date1').text(forecastDay1);
@@ -35,128 +41,127 @@ function renderDates() {
 };
 renderDates();
 
+// When the user submits their city in the search form input...
 $searchForm.submit(function(event) {
+    // ...prevent the default form submission behavior...
     event.preventDefault();
+    // ...pull the user's input value
     let userInput = event.currentTarget[0].value;
+    // ...if the user submitted without entering a city, stop running the function...
     if(!userInput) {
         return
     }
+    //...otherwise, run the getCityWeather function using the user's input city...
     getCityWeather(userInput);
+    // ...and reset the input field to blank.
     $('input').val("");
 });
 
+// Creating a function that renders the user's input city to the page...
 function getCityWeather(userCity) {
-fetch("https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=1a917358c02b3e63d3ca8a69be818363")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-        let longitude = data.coord.lon;
-        let latitude = data.coord.lat;
-        let city = data.name;
-        if($.inArray(city,searchedCityArray) < 0) {
-            searchedCityArray.push(city);
-            let newCityListItem = $('<li>');
-            let newCity = $('<button>');
-            newCity.text(city);
-            $('ul').append(newCityListItem);
-            newCityListItem.append(newCity);
-            newCity.click(function(event) {
-                console.log(event);
-                getCityWeather(event.target.innerText);
-            })
-        }
+    // First, use user's input city to fetch the Current Weather API data...
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=1a917358c02b3e63d3ca8a69be818363")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // Pulling the latitude and longitude coordinates and formatted city name from the API data
+            let longitude = data.coord.lon;
+            let latitude = data.coord.lat;
+            let city = data.name;
+            
+            // Checking if the searched city has already been searched for (and therefore is in the array of searched cities)...
+            if($.inArray(city,searchedCityArray) < 0) {
+                // ...if the city is a new search, push the city to the array of searched cities to eventually save to local storage
+                searchedCityArray.push(city);
+                // ...then add the searched city as a button to page under the list of previous searches
+                let newCityListItem = $('<li>');
+                let newCity = $('<button>');
+                newCity.text(city);
+                $('ul').append(newCityListItem);
+                newCityListItem.append(newCity);
+                // ....when the newly created button is clicked, it will generate the weather data for that city
+                newCity.click(function(event) {
+                    console.log(event);
+                    getCityWeather(event.target.innerText);
+                })
+            }
         
-        storeCity();
-        
-        $cityName.text(city);
+            // Run the function to store the array of searched cities to local storage
+            storeCity();
+            
+            // Render the searched city to the page in the forecast
+            $cityName.text(city);
 
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=imperial&appid=1a917358c02b3e63d3ca8a69be818363")
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                let currTemp = data.current.temp;
-                let currWind = data.current.wind_speed;
-                let currHumidity = data.current.humidity;
-                let currUVI = data.current.uvi;
-                let currIcon = data.current.weather[0].icon;
-                let currIconURL = "http://openweathermap.org/img/wn/"+currIcon+".png";
-                let daily = data.daily;
-                for(i=0; i<5; i++) {
-                    let dailyTemp = daily[i].temp.day;
-                    $forecastTemps[i].textContent = "Temp: " + dailyTemp + " °F";
-                    dailyTempArray.push(dailyTemp);
-                    let dailyHumidity = daily[i].humidity;
-                    $forecastHumiditys[i].textContent = "Humidity: " + dailyHumidity + " %";
-                    dailyHumidityArray.push(dailyHumidity);
-                    let dailyWind = daily[i].wind_speed;
-                    $forecastWinds[i].textContent = "Wind: " + dailyWind + " MPH";
-                    dailyWindArray.push(dailyWind);
-                    let dailyIcon = daily[i].weather[0].icon;
-                    let dailyIconURL = "http://openweathermap.org/img/wn/"+dailyIcon+".png";
-                    $forecastIcons[i].setAttribute("src",dailyIconURL);
-                    dailyIconArray.push(dailyIcon);
-                }
+            // Use the pulled latitude and longitudes for the user's input city to fetch One Call API data
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=imperial&appid=1a917358c02b3e63d3ca8a69be818363")
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    // Pulling current temperature, wind, humidity, UV data for the city
+                    let currTemp = data.current.temp;
+                    let currWind = data.current.wind_speed;
+                    let currHumidity = data.current.humidity;
+                    let currUVI = data.current.uvi;
+                    // Pulling the current conditions icon from the data and embedding it in a URL source so it can render to page
+                    let currIcon = data.current.weather[0].icon;
+                    let currIconURL = "http://openweathermap.org/img/wn/"+currIcon+".png";
 
-                $currIcon.attr('src',currIconURL);
-                $currTemp.text("  " + currTemp + " °F");
-                $currWind.text("  " + currWind + " MPH");
-                $currHumidity.text("  " + currHumidity + " %");
-                $currUV.text("  " + currUVI);
-                // CREATE IF STATEMENT TO STYLE UV COLOR
-                if(currUVI < 3) {
-                    $currUV.attr('class','low-uv')
-                } else if (currUVI >= 3 && currUVI < 5) {
-                    $currUV.attr('class','moderate-uv');
-                } else if (currUVI >= 5 && currUVI < 7) {
-                    $currUV.attr('class','high-uv');
-                } else if (currUVI >= 7 && currUVI < 9) {
-                    $currUV.attr('class','veryhigh-uv');
-                } else ($currUV.attr('class','extreme-uv'));
+                    // Rendering the current weather data to page
+                    $currIcon.attr('src',currIconURL);
+                    $currTemp.text("  " + currTemp + " °F");
+                    $currWind.text("  " + currWind + " MPH");
+                    $currHumidity.text("  " + currHumidity + " %");
+                    $currUV.text("  " + currUVI);
 
+                    // Conditional formatting of UV Index for low, moderate, high, very-high and extreme.
+                    // UVI range values sourced from https://www.epa.gov/sites/production/files/documents/uviguide.pdf
+                    if(currUVI < 3) {
+                        $currUV.attr('class','low-uv')
+                    } else if (currUVI >= 3 && currUVI < 5) {
+                        $currUV.attr('class','moderate-uv');
+                    } else if (currUVI >= 5 && currUVI < 7) {
+                        $currUV.attr('class','high-uv');
+                    } else if (currUVI >= 7 && currUVI < 9) {
+                        $currUV.attr('class','veryhigh-uv');
+                    } else ($currUV.attr('class','extreme-uv'));
 
-                // might just need the city name to save to local storage and then push it through as the variable
-                // let cityWeather = {
-                //     cityName: city,
-                //     // cityIcon: currIcon,
-                //     // cityTemp: currTemp,
-                //     // cityWind: currWind,
-                //     // cityHumidity: currHumidity,
-                //     // cityUVI: currUVI,
-                //     // fiveDayTemp: dailyTempArray,
-                //     // fiveDayWind: dailyWindArray,
-                //     // fiveDayHumidity: dailyHumidityArray,
-                //     // fiveDayIcon: dailyIconArray
-                // }
-                // currWeatherArray.push(cityWeather);
-                // console.log(currWeatherArray);
-                
-
-
-            });
-  });
+                    // Pulling the temp, humidity, wind, and icon for the next 5 days and rendering to page
+                    let daily = data.daily;
+                    for(i=0; i<5; i++) {
+                        let dailyTemp = daily[i].temp.day;
+                        $forecastTemps[i].textContent = "Temp: " + dailyTemp + " °F";
+                        dailyTempArray.push(dailyTemp);
+                        let dailyHumidity = daily[i].humidity;
+                        $forecastHumiditys[i].textContent = "Humidity: " + dailyHumidity + " %";
+                        dailyHumidityArray.push(dailyHumidity);
+                        let dailyWind = daily[i].wind_speed;
+                        $forecastWinds[i].textContent = "Wind: " + dailyWind + " MPH";
+                        dailyWindArray.push(dailyWind);
+                        let dailyIcon = daily[i].weather[0].icon;
+                        let dailyIconURL = "http://openweathermap.org/img/wn/"+dailyIcon+".png";
+                        $forecastIcons[i].setAttribute("src",dailyIconURL);
+                        dailyIconArray.push(dailyIcon);
+                    } 
+                });
+        });
 }
-// getCityWeather("atlanta");
 
-// $('.newSearchedCityBtn').click(getCityWeather(searchedCityArray[lastSearchIndex]));
-
-// submit event listener (make a form so enter is captured)
-// add get City Weather function which renders data to page
-// add storing city name
-// create list item with button type and append to ul under form
-
-// Creating a function to store an array of city weather data to local storage
+// Creating a function to store an array of searched cities to local storage
 function storeCity() {
     localStorage.setItem("city-name", JSON.stringify(searchedCityArray));
 }
 
+// Creating a function to render the saved cities to the page
 function renderSavedCities() {
     let storedCities = JSON.parse(localStorage.getItem("city-name"));
 
+    // If there are cities saved to local storage, set the array of searched cities to those
     if(storedCities) {
         searchedCityArray = storedCities;
         
+        // For each previously searched city saved in local storage, render it to the page as a button in a list
         for (let i=0; i < storedCities.length; i++) {
             let searchedCityLI = $('<li>')
             let searchedCityBtn = $('<button>').text(searchedCityArray[i]);
@@ -167,38 +172,17 @@ function renderSavedCities() {
         }
     }
 }
+// Show any saved city searches upon page load
 renderSavedCities()
 
-// add getItem function for pulling from local storage on page load
-// .....add rendering city names as list item buttons
-
-// add click listeners for all list item buttons that take name and input it into getCityWeather function as the input
+// When any of the previously searched cities' buttons are clicked, fetch and render the weather data for that city
 $('.searchedCityBtn').click(function(event) {
-    // console.log(event);
-    getCityWeather(event.target.value);
+        getCityWeather(event.target.value);
 });
 
-// $('.latestSearchCity').click(function(event) {
-//     console.log(event);
-//     getCityWeather(event.target.value);
-// });
-
-// function handleNewCityClick() {
-//     console.log(event);
-//     getCityWeather(event.target.value);
-
-
-// create a button to clear history
+// Creating a button that allows the user to clear the list of previously searched cities (which will clear local storage)
 $('#clearSearchDiv button').click(function() {
     $('ul li').hide();
-    // $('#clearSearchDiv').hide();
     localStorage.clear();
-    // location.reload();
     searchedCityArray = [];
 });
-
-// on page load set input value to Atlanta, then have submit event override it
-
-
-
-// UV Index conditional styling
